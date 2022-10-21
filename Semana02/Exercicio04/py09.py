e48d3578558fb254dde14f43b46a4c455c5f4a91
pecegl <106319139+pecegl@users.noreply.github.com>
2022-10-21 18:52:26 -0300

    Create py09.py

---
+++ Semana02/Exercicio04/py09.py
@@ -0,0 +1,42 @@
+# import my_module
+# import my_module as mm
+# from my_module import find_index as fi, test
+# from my_module import *
+from my_module import find_index, test
+
+import sys
+
+import random
+
+import math
+
+import datetime
+import calendar
+
+import os
+
+courses = ['History', 'Math', 'Physics', 'CompSci']
+
+# index = my_module.find_index(courses, 'Math')
+# index = mm.find_index(courses, 'Math')
+# index = fi(courses, 'Math')
+index = find_index(courses, 'Math')
+print(index)
+print(test)
+
+print(sys.path)
+
+random_course = random.choice(courses)
+print(random_course)
+
+rads = math.radians(90)
+print(rads)
+print(math.sin(rads))
+
+today = datetime.date.today()
+print(today)
+print(calendar.isleap(2017))
+print(calendar.isleap(2020))
+
+print(os.getcwd())
+print(os.__file__)
